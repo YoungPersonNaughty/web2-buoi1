c40ce7876ccdb071148f831cb4545ab542d1bb8b
phain <nie99418@donga.edu.vn>
2025-01-04 10:11:18 +0700

    Init project and add category functionalities

---
+++ src/main/java/com/example/web2buoi1/Web2Buoi1Application.java
@@ -0,0 +1,13 @@
+package com.example.web2buoi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2Buoi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web2Buoi1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web2buoi1/Web2Buoi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web2buoi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2Buoi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
